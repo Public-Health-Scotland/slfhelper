797228323c1ebd585e6fcd000407fd74afe9b2bb
James McMahon <jamesmcmahon0@gmail.com>
2019-07-04 16:35:35 +0100

    Adding drop parameter

--- R/get_anon_chi.R
+++ R/get_anon_chi.R
@@ -9,13 +9,18 @@
 #' @examples
 #' get_anon_chi(data)
 #' get_anon_chi(data, chi_var = "uniqueid")
-get_anon_chi <- function(data, chi_var = "chi") {
+get_anon_chi <- function(data, chi_var = "chi", drop = TRUE) {
   default_name <- "chi"
-  return(data %>%
+
+  data <- data %>%
     dplyr::left_join(
       fst::read_fst(
         "/conf/hscdiip/01-Source-linkage-files/CHI-to-Anon-lookup.fst"
       ),
       by = setNames(default_name, chi_var)
-    ))
+    )
+  if (drop) {
+    data <- data %>% dplyr::select(-{{chi_var}})
+  }
+  return(data)
 }
