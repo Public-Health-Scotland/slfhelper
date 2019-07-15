3de92f8a010e47b1562e45de7c3d66f656b21440
James McMahon <jamesmcmahon0@gmail.com>
2019-07-15 13:40:48 +0100

    Adding variable names as data for easy access

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Names of variables in the epsiode files
+#'
+#' @name ep_file_vars
+#' @docType data
+#' @keywords data
+"ep_file_vars"
+
+#' Names of variables in the individual files
+#'
+#' @name indiv_file_vars
+#' @docType data
+#' @keywords data
+"indiv_file_vars"
