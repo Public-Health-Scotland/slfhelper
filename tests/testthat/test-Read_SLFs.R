aaece4f9bce0b7c0ebf1ceac13cf533fa591ea86
James McMahon <jamesmcmahon0@gmail.com>
2019-07-19 09:30:02 +0100

    Add comments, randomly pick a year to test

--- tests/testthat/test-Read_SLFs.R
+++ tests/testthat/test-Read_SLFs.R
@@ -7,16 +7,30 @@ test_that("Year formats correctly", {
 })
 
 test_that("Reads individual", {
-  indiv_1718 <- read_slf_individual("1718")
+  # Read file, pick a year at random
+  year <- sample(c("1516", "1617", "1718", "1819"), 1)
+  indiv_1718 <- read_slf_individual(year)
+
   expect_type(indiv_1718, "list")
   expect_gte(nrow(indiv_1718), 4000000)
   expect_named(indiv_1718)
 })
 
 test_that("Reads episode", {
-  ep_1718 <- read_slf_episode("1718")
+  # Read file, pick a year at random
+  year <- sample(c("1516", "1617", "1718", "1819"), 1)
+  ep_1718 <- read_slf_episode(year)
+
   expect_type(ep_1718, "list")
   expect_gte(nrow(ep_1718), 10000000)
   expect_named(ep_1718)
 })
 
+test_that("column selection works", {
+  indiv_1718 <- read_slf_individual("1718", columns = c("anon_chi"))
+
+  expect_type(indiv_1718, "list")
+  expect_equal(length(indiv_1718), 1)
+  expect_gte(nrow(indiv_1718), 4000000)
+  expect_named(indiv_1718)
+})
