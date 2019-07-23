83d6b62c3bc751883d8e7b234baa552f124ffdfe
James McMahon <jamesmcmahon0@gmail.com>
2019-07-23 16:49:14 +0100

    minor restructure

--- R/get_chi.R
+++ R/get_chi.R
@@ -2,6 +2,7 @@
 #'
 #' @param data tibble or data frame
 #' @param anon_chi_var Anon CHI variable: the name of the variable containg anonymised CHI (default is anon_chi)
+#' @param drop Optional boolean indicating whether the existing anon_ch_var should be dropped - default is TRUE
 #'
 #' @return a tibble
 #' @export
@@ -11,20 +12,19 @@
 get_chi <- function(data, anon_chi_var = "anon_chi", drop = TRUE) {
   default_name <- "anon_chi"
 
+  chi_lookup <- fst::read_fst(
+    "/conf/hscdiip/01-Source-linkage-files/Anon-to-CHI-lookup.fst"
+  )
+
   data <- data %>%
     dplyr::left_join(
-      fst::read_fst(
-        "/conf/hscdiip/01-Source-linkage-files/Anon-to-CHI-lookup.fst"
-      ),
+      chi_lookup,
       by = setNames(default_name, anon_chi_var)
     )
 
   if (drop) {
-    data <- data %>% dplyr::select(-{
-      {
-        anon_chi_var
-      }
-    })
+    data <- data %>%
+      dplyr::select(-{{ anon_chi_var }})
   }
 
   return(data)
