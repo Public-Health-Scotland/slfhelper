a383078f0144bd26ff53d83d14c8170ed6cc766d
James McMahon <j.mcmahon1@nhs.net>
2019-09-11 14:54:46 +0100

    addding tests

--- tests/testthat/test-var_lists_match.R
+++ tests/testthat/test-var_lists_match.R
@@ -1,3 +1,13 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("episode file vars match the vars list", {
+  data_vars <- ep_file_vars
+  file_vars <- as.list(names(read_slf_episode("1819", from = 1, to = 1)))
+
+  expect_identical(data_vars, file_vars)
+})
+
+test_that("individual file vars match the vars list", {
+  data_vars <- ep_file_vars
+  file_vars <- as.list(names(read_slf_individual("1819", from = 1, to = 1)))
+
+  expect_identical(data_vars, file_vars)
 })
