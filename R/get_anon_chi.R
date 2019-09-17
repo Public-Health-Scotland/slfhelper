e30c5e4d5bd52311b47a268d67b0e3ac44ecd7d3
James McMahon <j.mcmahon1@nhs.net>
2019-09-17 14:36:39 +0100

    renamed from data to tibb to avoid potential conflicts (didn't fix the issue)

--- R/get_anon_chi.R
+++ R/get_anon_chi.R
@@ -1,6 +1,6 @@
 #' Match on anon_chi to a dataset using CHI numbers
 #'
-#' @param data tibble or data frame
+#' @param tibb tibble or data frame
 #' @param chi_var CHI variable: the name of the variable containing CHI (default is chi)
 #' @param drop Optional boolean indicating whether the existing chi_var should be dropped - default is TRUE
 #'
@@ -11,22 +11,22 @@
 #' get_anon_chi(data)
 #' get_anon_chi(data, drop = FALSE)
 #' get_anon_chi(data, chi_var = "unique_id")
-get_anon_chi <- function(data, chi_var = "chi", drop = TRUE) {
+get_anon_chi <- function(tibb, chi_var = "chi", drop = TRUE) {
   default_name <- "chi"
 
   anon_chi_lookup <- fst::read_fst(
     "/conf/hscdiip/01-Source-linkage-files/CHI-to-Anon-lookup.fst"
   )
 
-  data <- data %>%
+  tibb <- tibb %>%
     dplyr::left_join(
       anon_chi_lookup,
       by = stats::setNames(default_name, chi_var)
     )
 
   if (drop) {
-    data <- data %>%
+    tibb <- tibb %>%
       dplyr::select(-{{ chi_var }})
   }
-  return(data)
+  return(tibb)
 }
--- R/get_chi.R
+++ R/get_chi.R
@@ -1,6 +1,6 @@
 #' Match on CHI numbers to a dataset using anon_chi
 #'
-#' @param data tibble or data frame
+#' @param tibb tibble or data frame
 #' @param anon_chi_var Anon CHI variable: the name of the variable containing anonymised CHI (default is anon_chi)
 #' @param drop Optional boolean indicating whether the existing anon_ch_var should be dropped - default is TRUE
 #'
@@ -10,23 +10,23 @@
 #' @examples
 #' get_chi(data)
 #' get_chi(data, drop = FALSE)
-get_chi <- function(data, anon_chi_var = "anon_chi", drop = TRUE) {
+get_chi <- function(tibb, anon_chi_var = "anon_chi", drop = TRUE) {
   default_name <- "anon_chi"
 
   chi_lookup <- fst::read_fst(
     "/conf/hscdiip/01-Source-linkage-files/Anon-to-CHI-lookup.fst"
   )
 
-  data <- data %>%
+  tibb <- tibb %>%
     dplyr::left_join(
       chi_lookup,
       by = stats::setNames(default_name, anon_chi_var)
     )
 
   if (drop) {
-    data <- data %>%
+    tibb <- tibb %>%
       dplyr::select(-{{ anon_chi_var }})
   }
 
-  return(data)
+  return(tibb)
 }
