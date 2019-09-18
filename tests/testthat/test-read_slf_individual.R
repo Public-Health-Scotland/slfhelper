7ec3e85029ead0bf303a5bff1e50e2f5516b74a7
James McMahon <j.mcmahon1@nhs.net>
2019-09-18 16:20:22 +0100

    shrinking tests to make them run faster (should still catch fails)

--- tests/testthat/test-read_slf_individual.R
+++ tests/testthat/test-read_slf_individual.R
@@ -1,7 +1,7 @@
 test_that("Reads individual", {
   # Read file, pick a year at random
   year <- sample(c("1516", "1617", "1718", "1819"), 1)
-  indiv_file <- read_slf_individual(year, from = 1, to = 1000)
+  indiv_file <- read_slf_individual(year, from = 1, to = 100)
 
   # Test for anything odd
   expect_type(indiv_file, "list")
@@ -11,13 +11,13 @@ test_that("Reads individual", {
   expect_length(indiv_file, 155)
 
   # Test for roughly correct number of rows
-  expect_equal(nrow(indiv_file), 1000)
+  expect_equal(nrow(indiv_file), 100)
 })
 
 test_that("column selection works", {
   indiv_1718 <- read_slf_individual("1718",
                                     columns = c("anon_chi"),
-                                    from = 1, to = 1000)
+                                    from = 1, to = 100)
 
   # Test for anything odd
   expect_type(indiv_1718, "list")
@@ -30,7 +30,7 @@ test_that("column selection works", {
   expect_named(indiv_1718, "anon_chi")
 
   # Test for roughly correct number of rows
-  expect_equal(nrow(indiv_1718), 1000)
+  expect_equal(nrow(indiv_1718), 100)
 })
 
 test_that("read multiple years works", {
@@ -38,7 +38,7 @@ test_that("read multiple years works", {
   indiv <- read_slf_individual(c("1718", "1819"),
     columns = c("year", "anon_chi"),
     from = 1,
-    to = 100
+    to = 50
   )
 
   # Test for anything odd
@@ -48,22 +48,24 @@ test_that("read multiple years works", {
   expect_named(indiv, c("year", "anon_chi"))
   expect_length(indiv, 2)
 
-  # Test for the correct number of rows (100 * 2)
-  expect_equal(nrow(indiv), 200)
+  # Test for the correct number of rows (50 * 2)
+  expect_equal(nrow(indiv), 100)
 
-  # Test that we have 100 rows from each year
+  # Test that we have 50 rows from each year
   expect_equal(
     dplyr::count(indiv, year),
-    dplyr::tibble(year = c("1718", "1819"), n = c(100L, 100L))
+    dplyr::tibble(year = c("1718", "1819"), n = c(50L, 50L))
   )
 })
 
 test_that("Partnership filtering works", {
   indiv_1718_edinburgh <- read_slf_individual("1718",
                                               partnership = "S37000012",
-                                              columns = c("hscp2018"))
+                                              columns = c("hscp2018"),
+                                              from = 1,
+                                              to = 10000)
 
   expect_true(all(indiv_1718_edinburgh$hscp2018 == "S37000012"))
   expect_equal(length(unique(indiv_1718_edinburgh$hscp2018)), 1)
-  expect_gte(nrow(indiv_1718_edinburgh), 10000)
+  expect_gte(nrow(indiv_1718_edinburgh), 100)
 })
