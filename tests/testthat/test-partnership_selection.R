c7655a2c5ffa6780b022d7752bff7bc633b0bc92
James McMahon <j.mcmahon1@nhs.net>
2019-09-19 10:30:40 +0100

    Tests for multiple partnerships formatting

--- tests/testthat/test-partnership_selection.R
+++ tests/testthat/test-partnership_selection.R
@@ -1,11 +1,27 @@
 test_that("Partnership filtering works", {
   indiv_1718_edinburgh <- read_slf_individual("1718",
-                                              partnership = "S37000012",
-                                              columns = c("hscp2018"),
-                                              from = 1,
-                                              to = 10000)
+    partnership = "S37000012",
+    columns = c("hscp2018"),
+    from = 1,
+    to = 10000
+  )
 
   expect_true(all(indiv_1718_edinburgh$hscp2018 == "S37000012"))
   expect_equal(length(unique(indiv_1718_edinburgh$hscp2018)), 1)
   expect_gte(nrow(indiv_1718_edinburgh), 100)
+})
+
+
+test_that("Can select multiple partnerships", {
+  indiv_1718_edi_gla <- read_slf_individual("1718",
+    partnership = c("S37000012", "S37000015"),
+    columns = c("hscp2018"),
+    from = 1,
+    to = 10000
+  )
+
+  expect_true(all(indiv_1718_edi_gla$hscp2018 == "S37000012" |
+    indiv_1718_edi_gla$hscp2018 == "S37000015"))
+  expect_equal(length(unique(indiv_1718_edi_gla$hscp2018)), 2)
+  expect_gte(nrow(indiv_1718_edi_gla), 100)
 })
