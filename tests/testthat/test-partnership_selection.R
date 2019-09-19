067f06c72007755920f829430109be13f031d00b
James McMahon <j.mcmahon1@nhs.net>
2019-09-19 11:06:31 +0100

    Improving comments

--- tests/testthat/test-partnership_selection.R
+++ tests/testthat/test-partnership_selection.R
@@ -1,4 +1,5 @@
 test_that("Partnership filtering works", {
+  # Read in a bit of a file selecting only Edinburgh
   indiv_1718_edinburgh <- read_slf_individual("1718",
     partnership = "S37000012",
     columns = c("hscp2018"),
@@ -6,22 +7,28 @@ test_that("Partnership filtering works", {
     to = 10000
   )
 
+  # Should only have Edinburgh codes
   expect_true(all(indiv_1718_edinburgh$hscp2018 == "S37000012"))
+  # Should only be one unique code
   expect_equal(length(unique(indiv_1718_edinburgh$hscp2018)), 1)
+  # Should have at least 100 records (checks we're not getting an empty file)
   expect_gte(nrow(indiv_1718_edinburgh), 100)
 })
 
 
 test_that("Can select multiple partnerships", {
+  # Read in a bit of a file selecting Edinburgh and Glasgow
   indiv_1718_edi_gla <- read_slf_individual("1718",
     partnership = c("S37000012", "S37000015"),
     columns = c("hscp2018"),
     from = 1,
     to = 10000
   )
-
+  # Should only have Edinburgh or Glasgow city codes
   expect_true(all(indiv_1718_edi_gla$hscp2018 == "S37000012" |
     indiv_1718_edi_gla$hscp2018 == "S37000015"))
+  # Should be exactly 2 unique codes
   expect_equal(length(unique(indiv_1718_edi_gla$hscp2018)), 2)
+  # Should have at least 100 records (checks we're not getting an empty file)
   expect_gte(nrow(indiv_1718_edi_gla), 100)
 })
--- tests/testthat/test-var_lists_match.R
+++ tests/testthat/test-var_lists_match.R
@@ -1,13 +1,21 @@
 test_that("episode file vars match the vars list", {
+  # Get the list of episode file variables
   data_vars <- ep_file_vars
+
+  # Compare the variables in the current episode files to the list
   file_vars <- as.list(names(read_slf_episode("1819", from = 1, to = 1)))
 
+  # These should be identical (names, order etc.)
   expect_identical(data_vars, file_vars)
 })
 
 test_that("individual file vars match the vars list", {
-  data_vars <- ep_file_vars
+  # Get the list of individual file variables
+  data_vars <- indiv_file_vars
+
+  # Compare the variables in the current episode files to the list
   file_vars <- as.list(names(read_slf_individual("1819", from = 1, to = 1)))
 
+  # These should be identical (names, order etc.)
   expect_identical(data_vars, file_vars)
 })
