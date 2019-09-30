a09d4644e0c2be754cb26dc5547ff31335fa9e67
James McMahon <j.mcmahon1@nhs.net>
2019-09-30 11:14:55 +0100

    Updating to use the new chi_cohort file

--- tests/testthat/test-match_anon_chi.R
+++ tests/testthat/test-match_anon_chi.R
@@ -1,23 +1,7 @@
 test_that("Matching with a different name works", {
-  # Generate a sample which should contain at least some valid CHIs
-  set.seed(1)
-  n <- 100
-  # Call it UPI number
-  cohort <- dplyr::tibble(upi_number = paste0(
-    round(runif(n, min = 0, max = 3)),
-    round(runif(n, min = 0, max = 9)),
-    round(runif(n, min = 0, max = 1)),
-    round(runif(n, min = 0, max = 2)),
-    round(runif(n, min = 0, max = 9)),
-    round(runif(n, min = 0, max = 9)),
-    round(runif(n, min = 0, max = 9)),
-    round(runif(n, min = 0, max = 9)),
-    round(runif(n, min = 0, max = 9)),
-    round(runif(n, min = 0, max = 9))
-  ))
-
+  # Use the example chi cohort included
   # Match on anon_chi
-  cohort_with_anon <- cohort %>%
+  cohort_with_anon <- chi_cohort %>%
     get_anon_chi(chi_var = "upi_number")
 
   # Default behaviour is to drop the matching var
@@ -26,6 +10,6 @@ test_that("Matching with a different name works", {
   expect_true("anon_chi" %in% names(cohort_with_anon))
 
   # Number of rows and variables should be the same
-  expect_equal(nrow(cohort_with_anon), nrow(cohort))
-  expect_equal(length(cohort_with_anon), length(cohort))
+  expect_equal(nrow(cohort_with_anon), nrow(chi_cohort))
+  expect_equal(length(cohort_with_anon), length(chi_cohort))
 })
