db44616d2ce72f11101938a9d371365f16c13065
James McMahon <james.mcmahon@phs.scot>
2021-09-14 16:45:07 +0100

    Refactor function to create the file paths Now using `fs` for paths

---
+++ R/gen_file_path.R
@@ -0,0 +1,22 @@
+#' Generate a SLF file path
+#'
+#' @param year Year of the file to be read, you can specify multiple years
+#'  which will then be returned as one file
+#' @param file_version Version of the file (individual / episode)
+#'
+#' @return a [fs_path][fs::fs-package] object
+gen_file_path <- function(year, file_version) {
+  # Clean up year
+  # We want it in the format 1718
+  # This will handle numbers or strings of the form 201718 or 1718
+  year <- format_year(year)
+
+  file_name <- paste0("source-", file_version, "-file-20", year, ".fst")
+
+  file_path <-
+    fs::path(
+      "/conf", "hscdiip", "01-Source-linkage-files", file_name
+    )
+
+  return(file_path)
+}
---
+++ tests/testthat/test-gen_file_path.R
@@ -0,0 +1,70 @@
+test_that("Produces single episode file path", {
+  path <- gen_file_path("1718", "episode")
+
+  expect_identical(path, fs::path("/conf/hscdiip/01-Source-linkage-files/source-episode-file-201718.fst"))
+
+  expect_identical(fs::path_ext(path), "fst")
+
+  expect_true(fs::file_exists(path))
+})
+
+
+test_that("Produces single individual file path", {
+  path <- gen_file_path("1718", "individual")
+
+  expect_identical(path, fs::path("/conf/hscdiip/01-Source-linkage-files/source-individual-file-201718.fst"))
+
+  expect_identical(fs::path_ext(path), "fst")
+
+  expect_true(fs::file_exists(path))
+})
+
+test_that("Produces correct episode file paths for multiple years", {
+  path <- gen_file_path(c("1415",
+                          "1516",
+                          "1617",
+                          "1718",
+                          "1819",
+                          "1920",
+                          "2021"),
+                        "episode")
+
+  expect_identical(path, fs::path(c(
+    "/conf/hscdiip/01-Source-linkage-files/source-episode-file-201415.fst",
+    "/conf/hscdiip/01-Source-linkage-files/source-episode-file-201516.fst",
+    "/conf/hscdiip/01-Source-linkage-files/source-episode-file-201617.fst",
+    "/conf/hscdiip/01-Source-linkage-files/source-episode-file-201718.fst",
+    "/conf/hscdiip/01-Source-linkage-files/source-episode-file-201819.fst",
+    "/conf/hscdiip/01-Source-linkage-files/source-episode-file-201920.fst",
+    "/conf/hscdiip/01-Source-linkage-files/source-episode-file-202021.fst"
+  )))
+
+  expect_identical(fs::path_ext(path), rep_len("fst", length(path)))
+
+  expect_true(all(fs::file_exists(path)))
+})
+
+test_that("Produces correct individual file paths for multiple years", {
+  path <- gen_file_path(c("1415",
+                          "1516",
+                          "1617",
+                          "1718",
+                          "1819",
+                          "1920",
+                          "2021"),
+                        "individual")
+
+  expect_identical(path, fs::path(c(
+    "/conf/hscdiip/01-Source-linkage-files/source-individual-file-201415.fst",
+    "/conf/hscdiip/01-Source-linkage-files/source-individual-file-201516.fst",
+    "/conf/hscdiip/01-Source-linkage-files/source-individual-file-201617.fst",
+    "/conf/hscdiip/01-Source-linkage-files/source-individual-file-201718.fst",
+    "/conf/hscdiip/01-Source-linkage-files/source-individual-file-201819.fst",
+    "/conf/hscdiip/01-Source-linkage-files/source-individual-file-201920.fst",
+    "/conf/hscdiip/01-Source-linkage-files/source-individual-file-202021.fst"
+  )))
+
+  expect_identical(fs::path_ext(path), rep_len("fst", length(path)))
+
+  expect_true(all(fs::file_exists(path)))
+})
