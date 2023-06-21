136bd58f5f1c2f7c4ae8ef3837e9f16e281c65e4
James McMahon <james.mcmahon@phs.scot>
2023-06-21 11:43:18 +0100

    Read parquet files

--- R/gen_file_path.R
+++ R/gen_file_path.R
@@ -4,12 +4,18 @@
 #'
 #' @param call Supply environment from the top-level function to make any error
 #' messages more useful.
+#' @param dev `r lifecycle::badge("experimental")` Whether to get the file from
+#' the development area (`/conf/sourcedev/Source_Linkage_File_Updates`).
+#' The default (`FALSE`) will get the production file from the usual area.
+#' @param ext `r lifecycle::badge("questioning")` The file extension to read.
 #'
-#' @return a [fs_path][fs::fs-package] object
+#' @return a [fs_path][fs::fs-package] object.
 gen_file_path <- function(
     year,
     file_version,
-    call) {
+    call,
+    dev = FALSE,
+    ext = "fst") {
   # Clean up year
   # We want it in the format 1718
   # This will handle numbers or strings of the form 201718 or 1718
@@ -22,13 +28,26 @@ gen_file_path <- function(
 
   file_name <- stringr::str_glue("source-{file_version}-file-20{year}")
 
-  file_path <- fs::path(
-    "/conf",
-    "hscdiip",
-    "01-Source-linkage-files",
-    file_name,
-    ext = "fst"
-  )
+  if (dev) {
+    file_path <- fs::path(
+      "/",
+      "conf",
+      "sourcedev",
+      "Source_Linkage_File_Updates",
+      year,
+      stringr::str_glue("source-{file_version}-file-{year}"),
+      ext = ext
+    )
+  } else {
+    file_path <- fs::path(
+      "/",
+      "conf",
+      "hscdiip",
+      "01-Source-linkage-files",
+      stringr::str_glue("source-{file_version}-file-20{year}"),
+      ext = ext
+    )
+  }
 
   return(file_path)
 }
---
+++ R/read_slf_parquet.R
@@ -0,0 +1,148 @@
+#' Read a Source Linkage File (parquet)
+#'
+#' @description `r lifecycle::badge("experimental")`
+#'
+#' @inherit read_slf
+#' @inheritParams gen_file_path
+read_slf_parquet <- function(
+    year,
+    col_select = NULL,
+    file_version = c("episode", "individual"),
+    partnerships = NULL,
+    recids = NULL,
+    dev = TRUE) {
+  file_path <- gen_file_path(
+    year,
+    file_version,
+    call = rlang::caller_env(),
+    dev = dev,
+    ext = "parquet"
+  )
+
+  stopifnot(fs::file_access(file_path, "read"))
+
+  # If we're looking at the dev version of the file swap anon_chi for chi
+  if (dev && !is.null(col_select) && "anon_chi" %in% col_select) {
+    col_select[which(col_select == "anon_chi")] <- "chi"
+  }
+
+  # If the we are trying to filter by partnership or recid
+  # but the column wasn't selected we need to add it (and remove later)
+  remove_partnership_var <- FALSE
+  remove_recid_var <- FALSE
+  if (!is.null(col_select)) {
+    if (!is.null(partnerships) &
+      !("hscp2018" %in% col_select)) {
+      col_select <- c(col_select, "hscp2018")
+      remove_partnership_var <- TRUE
+    }
+    if (!is.null(recids) & file_version == "episode" &
+      !("recid" %in% col_select)) {
+      col_select <- c(col_select, "recid")
+      remove_recid_var <- TRUE
+    }
+  }
+
+  slf_table <- purrr::map(
+    file_path,
+    function(file_path) {
+      slf_table <- arrow::read_parquet(
+        file = file_path,
+        col_select = arrow::all_of(col_select),
+        as_data_frame = FALSE
+      )
+
+      if (!is.null(recids)) {
+        slf_table <- dplyr::filter(
+          slf_table,
+          .data$recid %in% recids
+        )
+      }
+      if (!is.null(partnerships)) {
+        slf_table <- dplyr::filter(
+          slf_table,
+          .data$partnership %in% partnerships
+        )
+      }
+      if (remove_partnership_var) {
+        slf_table <- dplyr::select(slf_table, -"hscp2018")
+      }
+      if (remove_recid_var) {
+        slf_table <- dplyr::select(slf_table, -"recid")
+      }
+
+      return(dplyr::collect(slf_table))
+    }
+  )
+
+  return(purrr::list_rbind(slf_table))
+}
+
+#' Read a Source Linkage episode file (parquet)
+#'
+#' @description `r lifecycle::badge("experimental")`
+#'
+#' @inherit read_slf_episode
+#' @inheritParams read_slf_parquet
+read_slf_episode_parquet <- function(
+    year,
+    col_select = NULL,
+    partnerships = NULL,
+    recids = NULL,
+    as_data_frame = TRUE,
+    dev = TRUE,
+    columns = lifecycle::deprecated()) {
+  if (lifecycle::is_present(columns)) {
+    lifecycle::deprecate_soft(
+      "0.10.0",
+      "read_slf_episode(columns)",
+      "read_slf_episode(col_select)"
+    )
+    col_select <- columns
+  }
+  # TODO add option to drop blank CHIs?
+  # TODO add a filter by recid option
+  return(
+    read_slf_parquet(
+      year = year,
+      col_select = unique(col_select),
+      file_version = "episode",
+      partnerships = unique(partnerships),
+      recids = unique(recids)
+    )
+  )
+}
+
+#' Read a Source Linkage individual file (parquet)
+#'
+#' @description `r lifecycle::badge("experimental")`
+#'
+#' @inherit read_slf_individual
+#' @inheritParams read_slf_parquet
+read_slf_individual_parquet <-
+  function(year,
+           col_select = NULL,
+           partnerships = NULL,
+           recids = NULL,
+           as_data_frame = TRUE,
+           dev = TRUE,
+           columns = lifecycle::deprecated()) {
+    if (lifecycle::is_present(columns)) {
+      lifecycle::deprecate_soft(
+        "0.10.0",
+        "read_slf_individual(columns)",
+        "read_slf_individual(col_select)"
+      )
+      col_select <- columns
+    }
+    return(
+      read_slf_parquet(
+        year = year,
+        file_version = "individual",
+        partnerships = unique(partnerships),
+        col_select = unique(col_select),
+        dev = TRUE,
+        ...
+      )
+    )
+  }
