f05d1283274872ac7dcfb0c11a9c1f62f6fbf68a
Moohan <Moohan@users.noreply.github.com>
2023-07-19 09:00:39 +0000

    Style with {styler} (GitHub action)

--- R/get_anon_chi.R
+++ R/get_anon_chi.R
@@ -5,7 +5,7 @@
 #' (default is chi).
 #' @param drop Optional boolean indicating whether the existing `chi_var`
 #' should be dropped - default is TRUE.
-#' @param check Optional boolean, should the CHIs be checked using 
+#' @param check Optional boolean, should the CHIs be checked using
 #' [phsmethods::chi_check()] - default is TRUE.
 #'
 #' @importFrom rlang :=
@@ -20,34 +20,34 @@
 #' }
 get_anon_chi <- function(chi_cohort, chi_var = "chi", drop = TRUE, check = TRUE) {
   if (check) {
-  # Optional code, if the user has phsmethods installed check the CHIs with it.
-  if (rlang::is_installed("phsmethods")) {
-    checked_chi <- phsmethods::chi_check(
-      dplyr::pull(chi_cohort, {{ chi_var }})
-    )
-
-    which_invalid <- which(
-      !(checked_chi %in% c("Valid CHI", "Missing (Blank)", "Missing (NA)"))
-    )
-
-    n_invalid <- length(which_invalid)
-
-    if (n_invalid > 10) {
-      cli::cli_warn(c(
-        "More than 10 of the CHIs supplied look invalid,",
-        "you should check them with {.fn phsmethods::chi_check}"
-      ))
-    } else if (n_invalid > 0) {
-      cli::cli_alert_warning(
-        "Some of the CHI numbers supplied look invalid according to {.fn phsmethods::chi_check}"
+    # Optional code, if the user has phsmethods installed check the CHIs with it.
+    if (rlang::is_installed("phsmethods")) {
+      checked_chi <- phsmethods::chi_check(
+        dplyr::pull(chi_cohort, {{ chi_var }})
       )
-      print(tibble::tibble(
-        {{ chi_var }} := dplyr::pull(chi_cohort, {{ chi_var }})[which_invalid],
-        reason = checked_chi[which_invalid]
-      ))
+
+      which_invalid <- which(
+        !(checked_chi %in% c("Valid CHI", "Missing (Blank)", "Missing (NA)"))
+      )
+
+      n_invalid <- length(which_invalid)
+
+      if (n_invalid > 10) {
+        cli::cli_warn(c(
+          "More than 10 of the CHIs supplied look invalid,",
+          "you should check them with {.fn phsmethods::chi_check}"
+        ))
+      } else if (n_invalid > 0) {
+        cli::cli_alert_warning(
+          "Some of the CHI numbers supplied look invalid according to {.fn phsmethods::chi_check}"
+        )
+        print(tibble::tibble(
+          {{ chi_var }} := dplyr::pull(chi_cohort, {{ chi_var }})[which_invalid],
+          reason = checked_chi[which_invalid]
+        ))
+      }
     }
   }
-    }
 
   lookup <- tibble::tibble(
     chi = unique(chi_cohort[[chi_var]])
