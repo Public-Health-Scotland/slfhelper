e3484086ff044acd9c1e43a7fcded7b6f892fad7
James McMahon <james.mcmahon@phs.scot>
2023-07-19 09:58:27 +0100

    Make the check on the CHIs optional

--- R/get_anon_chi.R
+++ R/get_anon_chi.R
@@ -1,10 +1,12 @@
 #' Match on anon_chi to a dataset using CHI numbers
 #'
-#' @param chi_cohort a [tibble][tibble::tibble-package] or data frame
+#' @param chi_cohort a [tibble][tibble::tibble-package] or data frame.
 #' @param chi_var CHI variable: the name of the variable containing CHI
-#' (default is chi)
+#' (default is chi).
 #' @param drop Optional boolean indicating whether the existing `chi_var`
-#' should be dropped - default is TRUE
+#' should be dropped - default is TRUE.
+#' @param check Optional boolean, should the CHIs be checked using 
+#' [phsmethods::chi_check()] - default is TRUE.
 #'
 #' @importFrom rlang :=
 #'
@@ -16,7 +18,8 @@
 #' chi_cohort %>% get_anon_chi()
 #' chi_cohort %>% get_anon_chi(chi_var = "upi_number")
 #' }
-get_anon_chi <- function(chi_cohort, chi_var = "chi", drop = TRUE) {
+get_anon_chi <- function(chi_cohort, chi_var = "chi", drop = TRUE, check = TRUE) {
+  if (check) {
   # Optional code, if the user has phsmethods installed check the CHIs with it.
   if (rlang::is_installed("phsmethods")) {
     checked_chi <- phsmethods::chi_check(
@@ -44,6 +47,7 @@ get_anon_chi <- function(chi_cohort, chi_var = "chi", drop = TRUE) {
       ))
     }
   }
+    }
 
   lookup <- tibble::tibble(
     chi = unique(chi_cohort[[chi_var]])
