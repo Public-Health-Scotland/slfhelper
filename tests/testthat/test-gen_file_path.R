2899044415ecd2bfc4cd53c9a95e9d12c7af8f38
James McMahon <james.mcmahon@phs.scot>
2023-07-20 13:13:03 +0100

    Update test for new file paths

--- tests/testthat/test-gen_file_path.R
+++ tests/testthat/test-gen_file_path.R
@@ -1,26 +1,26 @@
 test_that("Produces single episode file path", {
   path <- gen_file_path("1718", "episode")
 
-  expect_identical(path, fs::path(
+  expect_equal(path, fs::path(
     "/conf/hscdiip",
     "01-Source-linkage-files",
-    "source-episode-file-201718.fst"
+    "source-episode-file-1718.fst"
   ))
 
-  expect_identical(fs::path_ext(path), "fst")
+  expect_equal(fs::path_ext(path), "fst")
 })
 
 
 test_that("Produces single individual file path", {
   path <- gen_file_path("1718", "individual")
 
-  expect_identical(path, fs::path(
+  expect_equal(path, fs::path(
     "/conf/hscdiip",
     "01-Source-linkage-files",
-    "source-individual-file-201718.fst"
+    "source-individual-file-1718.fst"
   ))
 
-  expect_identical(fs::path_ext(path), "fst")
+  expect_equal(fs::path_ext(path), "fst")
 })
 
 test_that("Produces correct episode file paths for multiple years", {
@@ -37,17 +37,20 @@ test_that("Produces correct episode file paths for multiple years", {
     "episode"
   )
 
-  expect_identical(path, fs::path(c(
-    "/conf/hscdiip/01-Source-linkage-files/source-episode-file-201415.fst",
-    "/conf/hscdiip/01-Source-linkage-files/source-episode-file-201516.fst",
-    "/conf/hscdiip/01-Source-linkage-files/source-episode-file-201617.fst",
-    "/conf/hscdiip/01-Source-linkage-files/source-episode-file-201718.fst",
-    "/conf/hscdiip/01-Source-linkage-files/source-episode-file-201819.fst",
-    "/conf/hscdiip/01-Source-linkage-files/source-episode-file-201920.fst",
-    "/conf/hscdiip/01-Source-linkage-files/source-episode-file-202021.fst"
-  )))
+  expect_equal(
+    path,
+    fs::path(c(
+      "/conf/hscdiip/01-Source-linkage-files/source-episode-file-1415.fst",
+      "/conf/hscdiip/01-Source-linkage-files/source-episode-file-1516.fst",
+      "/conf/hscdiip/01-Source-linkage-files/source-episode-file-1617.fst",
+      "/conf/hscdiip/01-Source-linkage-files/source-episode-file-1718.fst",
+      "/conf/hscdiip/01-Source-linkage-files/source-episode-file-1819.fst",
+      "/conf/hscdiip/01-Source-linkage-files/source-episode-file-1920.fst",
+      "/conf/hscdiip/01-Source-linkage-files/source-episode-file-2021.fst"
+    ))
+  )
 
-  expect_identical(fs::path_ext(path), rep_len("fst", length(path)))
+  expect_equal(fs::path_ext(path), rep_len("fst", length(path)))
 })
 
 test_that("Produces correct individual file paths for multiple years", {
@@ -64,15 +67,18 @@ test_that("Produces correct individual file paths for multiple years", {
     "individual"
   )
 
-  expect_identical(path, fs::path(c(
-    "/conf/hscdiip/01-Source-linkage-files/source-individual-file-201415.fst",
-    "/conf/hscdiip/01-Source-linkage-files/source-individual-file-201516.fst",
-    "/conf/hscdiip/01-Source-linkage-files/source-individual-file-201617.fst",
-    "/conf/hscdiip/01-Source-linkage-files/source-individual-file-201718.fst",
-    "/conf/hscdiip/01-Source-linkage-files/source-individual-file-201819.fst",
-    "/conf/hscdiip/01-Source-linkage-files/source-individual-file-201920.fst",
-    "/conf/hscdiip/01-Source-linkage-files/source-individual-file-202021.fst"
-  )))
+  expect_equal(
+    path,
+    fs::path(c(
+      "/conf/hscdiip/01-Source-linkage-files/source-individual-file-1415.fst",
+      "/conf/hscdiip/01-Source-linkage-files/source-individual-file-1516.fst",
+      "/conf/hscdiip/01-Source-linkage-files/source-individual-file-1617.fst",
+      "/conf/hscdiip/01-Source-linkage-files/source-individual-file-1718.fst",
+      "/conf/hscdiip/01-Source-linkage-files/source-individual-file-1819.fst",
+      "/conf/hscdiip/01-Source-linkage-files/source-individual-file-1920.fst",
+      "/conf/hscdiip/01-Source-linkage-files/source-individual-file-2021.fst"
+    ))
+  )
 
-  expect_identical(fs::path_ext(path), rep_len("fst", length(path)))
+  expect_equal(fs::path_ext(path), rep_len("fst", length(path)))
 })
